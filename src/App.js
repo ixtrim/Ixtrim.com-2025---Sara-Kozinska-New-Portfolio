0d1a576c7f15843bfa29533a752d80af1bab873f
ixtrim <contact@ixtrim.com>
2024-03-17 01:11:32 +0100

    Global styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import { ThemeProvider, Global, css } from '@emotion/react';
 import theme from './theme';
@@ -5,86 +6,44 @@ import VideoBackground from './components/layout/VideoBackground';
 import Link from './components/common/Link';
 import Button from './components/common/Button';
 
+const globalStyles = css`
+  body {
+    font-family: ${theme.fonts.body};
+    font-size: ${theme.fontSizes.base};
+    letter-spacing: 1px;
+    line-height: 1.5em;
+    background-color: ${theme.colors.dark};
+    color: ${theme.colors.white};
+  }
+`;
+
+const appContainerStyles = css`
+  width: 100%;
+  min-height: 100vh;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  z-index: 1;
+  display: block;
+`;
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Global
-        styles={css`
-          body {
-            font-family: ${theme.fonts.body};
-            font-size: ${theme.fontSizes.base};
-            letter-spacing: 1px;
-            line-height: 1.5em;
-            background-color: ${theme.colors.dark};
-            color: ${theme.colors.white};
-          }
-        `}
-      />
+      <Global styles={globalStyles} />
       <VideoBackground />
-      <div className="App"><center>
-        <p>Whoever you are, your presence here means that I may have (or be) something you are looking for. But before we get down to business, let me introduce myself.</p>
-
-        <p>My name is Sara Kozińska. I was born on 21.10.1987 in Warsaw, Poland. Currently I live in sunny Barcelona, Spain, where I live with my stubborn (but still cute) French bulldog Lilo.</p>
-
-        <p>Since I can remember I have 3 passions: animals, boards (skateboard, snowboard, wakeboard, surfing...) and computers. To this day they are all part of my life. Over time, other interesting things were added, such as cooking (Mexican and Asian cuisine), drumming, music, filmmaking, traveling and self-development (spiritual development).</p>
-
-        <Button variant="outlined" size="small" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-<br/><br/>
-        <Button variant="outlined" size="medium" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="outlined" size="large" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="red" size="small" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-<br/><br/>
-        <Button variant="red" size="medium" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="red" size="large" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="orange" size="small" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-<br/><br/>
-        <Button variant="orange" size="medium" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="orange" size="large" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Button variant="grey" size="small" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-<br/><br/>
-        <Button variant="grey" size="medium" onClick={() => console.log('Clicked!')}>
-          Click Me
-        </Button>
-        <br/><br/>
-        <Link href="https://example.com" target="_blank" rel="noopener noreferrer" title="Example Site" variant="white" size="small">
-          Click Me
-        </Link>
-        <br/><br/>
-        <Link href="https://example.com" target="_blank" rel="noopener noreferrer" title="Example Site" variant="white" size="medium">
-          Click Me
-        </Link>
-        <br/><br/>
-        <Link href="https://example.com" target="_blank" rel="noopener noreferrer" title="Example Site" variant="white" size="large">
-          Click Me
-        </Link>
-        <br/><br/>
+      <div css={appContainerStyles}>
+        {/* Your content */}
+        <center>
+          {/* Example content and components */}
+          <p>Whoever you are, your presence here means that I may have (or be) something you are looking for...</p>
+          <Button variant="outlined" size="medium" onClick={() => console.log('Clicked!')}>
+            Click Me
+          </Button>
+          <Link href="https://example.com" target="_blank" rel="noopener noreferrer" title="Example Site" variant="white" size="medium">
+            Visit Example Site
+          </Link>
         </center>
       </div>
     </ThemeProvider>
@@ -92,3 +51,4 @@ function App() {
 }
 
 export default App;
+
